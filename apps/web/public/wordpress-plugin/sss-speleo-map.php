df1b59333ffa85dcee609c56e414cecc3370b92f
Danko <dankom@t-systems.com>
2026-08-13 09:52:26 +0200

    fix(wp): update WordPress plugin php class attribute and rebuild plugin zip

--- apps/web/public/wordpress-plugin/sss-speleo-map.php
+++ apps/web/public/wordpress-plugin/sss-speleo-map.php
@@ -28,7 +28,7 @@ function sss_speleo_map_shortcode_handler($atts) {
     ), $atts, 'sss_speleo_map');
 
     // Base URL of the hosted SSS Map application (adjust URL to your hosted domain/subfolder)
-    $base_url = !empty($atts['src']) ? $atts['src'] : 'http://localhost:3001/';
+    $base_url = !empty($atts['src']) ? $atts['src'] : 'https://dankez.github.io/sss_clubmap/';
 
     $query_args = array(
         'embed' => 'true'
@@ -44,7 +44,7 @@ function sss_speleo_map_shortcode_handler($atts) {
 
     $iframe_src = add_query_arg($query_args, $base_url);
 
-    $html = '<div className="sss-speleo-map-container" style="width:100%;margin:1.5rem 0;">';
+    $html = '<div class="sss-speleo-map-container" style="width:100%;margin:1.5rem 0;">';
     $html .= '<iframe src="' . esc_url($iframe_src) . '" ';
     $html .= 'style="width:' . esc_attr($atts['width']) . ';height:' . esc_attr($atts['height']) . ';border:none;border-radius:12px;box-shadow:0 8px 24px rgba(0,0,0,0.15);" ';
     $html .= 'allow="geolocation; fullscreen" allowfullscreen></iframe>';
